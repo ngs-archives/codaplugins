ef1931f808b503045d63e6b79de7a436897b1218
Atsushi NAGASE <atsn.ngs@gmail.com>
2012-12-22 16:21:26 +0900

    update wsinit

--- wsinit.sh
+++ wsinit.sh
@@ -4,10 +4,10 @@ git submodule update --init --recursive
 
 if [ ! -f gist/Gist/GistPlugin-APIKey.h ];
 then
-  cp ~/Documents/DropBox/Codes/GistPlugin-APIKey.h gist/Gist/GistPlugin-APIKey.h
+  cp ~/DropBox/Codes/GistPlugin-APIKey.h gist/Gist/GistPlugin-APIKey.h
 fi
 
 if [ ! -f evernote/Evernote/EvernotePlugin-APIKey.h ];
 then
-  cp ~/Documents/DropBox/Codes/EvernotePlugin-APIKey.h evernote/Evernote/EvernotePlugin-APIKey.h
+  cp ~/DropBox/Codes/EvernotePlugin-APIKey.h evernote/Evernote/EvernotePlugin-APIKey.h
 fi
